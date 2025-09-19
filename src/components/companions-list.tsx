1fde4fcd55d797bbb2dc5169272c4570a345c54d
Benedict San Juan <benedict_sanjuan@dlsl.edu.ph>
2025-09-19 19:17:27 +0800

    feat: update companions list table headers and sample row data

--- src/components/companions-list.tsx
+++ src/components/companions-list.tsx
@@ -23,17 +23,17 @@ const CompanionsList = ({title, companions, classNames} :CompanionsListProps) =>
         <TableCaption>A list of your recent sessions.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead>Subject</TableHead>
+            <TableHead>Topic</TableHead>
             <TableHead className="text-right">Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
+            <TableCell>Math</TableCell>
+            <TableCell>Algebra</TableCell>
             <TableCell className="text-right">$250.00</TableCell>
           </TableRow>
         </TableBody>
